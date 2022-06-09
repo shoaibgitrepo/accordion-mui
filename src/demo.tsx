856a30c6e22da65d863ae98a8d89df962a97ae3a
shoaibgitrepo <58928713+shoaibgitrepo@users.noreply.github.com>
2022-06-09 20:11:51 +0530

    Update demo.tsx

--- src/demo.tsx
+++ src/demo.tsx
@@ -4,16 +4,29 @@ import AccordionDetails from "@mui/material/AccordionDetails";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import { items } from "./fakeData";
+import items from "./fakeData";
+import { Button } from "@mui/material";
 
 export default function ControlledAccordions() {
-  const [expanded, setExpanded] = React.useState<boolean>(true);
+  const [expanded, setExpanded] = React.useState<string[]>([]);
+  const expandedAll: boolean = items.length === expanded.length;
 
   const handleChange = (panel: string) => (
     event: React.SyntheticEvent,
     isExpanded: boolean
   ) => {
-    setExpanded(!expanded);
+    const expandedInState: string[] = JSON.parse(JSON.stringify(expanded));
+    if (isExpanded) expandedInState.push(panel);
+    else {
+      const index: number = expandedInState.findIndex(
+        (collapsed) => collapsed === panel
+      );
+      if (index > -1) {
+        expandedInState.splice(index, 1);
+      }
+    }
+
+    setExpanded(expandedInState);
   };
 
   console.log("items", items);
@@ -41,33 +54,44 @@ export default function ControlledAccordions() {
   }
 
   return (
-    <div>
-      {data.map(({ name, status, description }) => (
-        <Accordion expanded={expanded} onChange={handleChange("panel1")}>
+    <div style={{ margin: 100, marginTop: 20, maxWidth: "50%" }}>
+      <Button
+        style={{ margin: 20, marginLeft: 0 }}
+        size="small"
+        variant="contained"
+        onClick={() =>
+          setExpanded(expandedAll ? [] : items.map(({ name }) => name))
+        }
+      >
+        {expandedAll ? "Collapse All" : "Expand All"}
+      </Button>
+      {items.map(({ name, details }) => (
+        <Accordion
+          style={{ marginBottom: 5, lineHeight: 10 }}
+          expanded={expanded.includes(name)}
+          onChange={handleChange(name)}
+        >
           <AccordionSummary
             expandIcon={<ExpandMoreIcon />}
             aria-controls="panel1bh-content"
             id="panel1bh-header"
           >
-            <Typography sx={{ width: "33%", flexShrink: 0 }}>Name 1</Typography>
+            <Typography sx={{ width: "33%", flexShrink: 0 }}>{name}</Typography>
             <Typography sx={{ color: "text.secondary" }}>
               In Progress
             </Typography>
           </AccordionSummary>
           <AccordionDetails>
             <Typography>
-              <p style={{ margin: 0 }}>UCC Description</p>
-              <p style={{ margin: 0 }}>UCC Description</p>
-              <p style={{ margin: 0 }}>UCC Description</p>
-              <p style={{ margin: 0 }}>UCC Description</p>
-              <p style={{ margin: 0 }}>UCC Description</p>
-              <p style={{ margin: 0 }}>UCC Description</p>
-              <p style={{ margin: 0 }}>UCC Description</p>
-              <p style={{ margin: 0 }}>UCC Description</p>
-              <p style={{ margin: 0 }}>UCC Description</p>
-              <p style={{ margin: 0 }}>UCC Description</p>
-              <p style={{ margin: 0 }}>UCC Description</p>
-              <p style={{ margin: 0 }}>UCC Description</p>
+              <table width="100%">
+                {details.map(({ productName, jurisdictionName }) => (
+                  <tr style={{ border: "solid 1px black" }}>
+                    <td>{productName}</td>
+                    <td>{jurisdictionName}</td>
+                    <td>-</td>
+                  </tr>
+                ))}
+              </table>
             </Typography>
           </AccordionDetails>
         </Accordion>
